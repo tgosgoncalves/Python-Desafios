6252f90ba470d140966dbbd9b68c959de4a51824
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-10-23 01:41:44 -0300

    Add files via upload

---
+++ desafios_python/Coletando pontos de dados/coletando_pontos_de_dados.py
@@ -0,0 +1,8 @@
+minimum_point = 100
+data_points = 150
+
+if data_points >= minimum_point:
+    print('There are enough data points!')
+
+if data_points < minimum_point:
+    print('Keep collecting data.')
